ebd254ed717bdd4e7d76a29419d6bd3a1b338b54
AnotherDole <rpwtd@yahoo.com>
2015-01-03 02:44:50 -0500

    More OpenShift configuration

--- server.js
+++ server.js
@@ -2,12 +2,11 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
-var fs = require('fs');
 var logic = require('./logic.js');
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
-server.listen(port, function(){
+server.listen(port,server_ip_address, function(){
 	console.log('Server listening on ' + server_ip_address + ' on port ' + port);
 });
 
