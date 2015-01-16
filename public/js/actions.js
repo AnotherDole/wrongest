9ac98c6b233aaf3b4f93506c7d26548cf92741aa
Lemon <lemon@hirelemon.com>
2015-01-16 00:02:24 -0600

    Three Intro Screens
    Join/Create Fork, and then the resulting screens.

---
+++ public/js/actions.js
@@ -0,0 +1,1 @@
+//@prepros-prepend partials/_setup.js
---
+++ public/js/partials/_setup.js
@@ -0,0 +1,20 @@
+/* jshint -W117 */
+$('#CreateRoomButton').click(function() {
+  $('.lead-in').hide();
+  $('.create-or-join').addClass('hidden');
+  $('.create-screen').removeClass('hidden');
+});
+$('#JoinRoomButton').click(function() {
+  $('.lead-in').hide();
+  $('.create-or-join').addClass('hidden');
+  $('.join-screen').removeClass('hidden');
+});
+
+$('#createPasswordInput').keyup(function() {
+  console.log($(this).val());
+  if ( $(this).val() == "") {
+    $(this).parent().next().children().removeClass('fa-lock').addClass('fa-unlock');
+  } else {
+    $(this).parent().next().children().removeClass('fa-unlock').addClass('fa-lock');
+  }
+});
