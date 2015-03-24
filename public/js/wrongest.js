4e55343ec8fe60e50dcf45cbfc3a418e5d6d9ea8
Lemon <lemon@hirelemon.com>
2015-03-23 22:22:52 -0500

    Setting the project up to handle minified js when the time comes.

---
+++ public/js/wrongest.js
@@ -0,0 +1,6 @@
+// Eventually when we flip the switch, I'd like to roll up the minified code, which is what this makes.
+// Using Prepros, results end up in js/min/wrongest.min.js
+
+//@prepros-prepend functions.js
+//@prepros-prepend actions.js
+//@prepros-prepend sockets.js
