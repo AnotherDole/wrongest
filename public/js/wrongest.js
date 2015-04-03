2cedb5142bbd0d2126fb4e7d52b0e2333e282579
Lemon <lemon@hirelemon.com>
2015-04-03 13:59:36 -0500

    Concatonated, minified Javascript (though I'm keeping the socket.io and jquery CDNs, because that's probably faster). Closes #40

--- public/js/wrongest.js
+++ public/js/wrongest.js
@@ -1,6 +1,8 @@
 // Eventually when we flip the switch, I'd like to roll up the minified code, which is what this makes.
 // Using Prepros, results end up in js/min/wrongest.min.js
 
+//@prepros-prepend vendor/jquery.knob.min.js
 //@prepros-prepend functions.js
 //@prepros-prepend actions.js
 //@prepros-prepend sockets.js
+//@prepros-prepend analytics.js
