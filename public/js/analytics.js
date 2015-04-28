b5ea25e3c921efb5156890cc6d4ec3376c35486f
Lemon <lemon@hirelemon.com>
2015-04-28 18:08:42 -0500

    Safer watch.

--- public/js/analytics.js
+++ public/js/analytics.js
@@ -1,10 +1,11 @@
-var gaTracking = false;
-if (window.location.hostname == "www.wrongest.net") {
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-  ga('create', 'UA-15129990-12', 'auto');
-  ga('send', 'pageview');
-  gaTracking = true;
+var gaTracking = true;
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+ga('create', 'UA-15129990-12', 'auto');
+ga('send', 'pageview');
+gaTracking = true;
+if (window.location.hostname == "localhost") {
+  var gaTracking = false;
 }
